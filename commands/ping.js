e86164a244a5bb4567ae686669f1245549b0830d
NoLeoCargado <63430679+Imperial29@users.noreply.github.com>
2025-08-16 11:18:31 -0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,13 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js');
-const safeExecute = require('../utils/safeExecute');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Responde con Pong!'),
-    async execute(interaction , client) {
-        await safeExecute(interaction, async (interaction) => {
-        await interaction.reply('Pong!');       
-     }); // <-- Cierra safeExecute
-    }, // <-- Cierra la función execute
-}; // <-- Cierra module.exports
+    async execute(interaction) {
+        await interaction.reply('Pong!');
+    },
+};
