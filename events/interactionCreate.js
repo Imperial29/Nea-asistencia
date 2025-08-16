1a013e6373be7f7633b9db2bb5fd699217e7b8d1
NoLeoCargado <63430679+Imperial29@users.noreply.github.com>
2025-08-16 01:26:36 -0600

    Update interactionCreate.js V2
    Se agrego una proteccion para el error de comando desconocido

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,17 +1,21 @@
 module.exports = {
     name: 'interactionCreate',
     async execute(interaction, client) {
-        if (!interaction.isCommand()) return;
+        if (!interaction.isChatInputCommand()) return;
 
         const command = client.commands.get(interaction.commandName);
-
         if (!command) return;
 
         try {
             await command.execute(interaction);
         } catch (error) {
             console.error(error);
-            await interaction.reply({ content: 'Hubo un error al ejecutar este comando!', ephemeral: true });
+
+            if (interaction.deferred || interaction.replied) {
+                await interaction.followUp({ content: 'Oh No Hubo Un Error Al Ejecutar Este Comando!', ephemeral: true });
+            } else {
+                await interaction.reply({ content: 'Ay Ya me dio amsiedad Hubo un error al ejecutar este comando!', ephemeral: true });
+            }
         }
     },
 };
