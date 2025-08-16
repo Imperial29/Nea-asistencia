98756867bc1961e01269993bcfa2f7c8d3b12378
NoLeoCargado <63430679+Imperial29@users.noreply.github.com>
2025-08-16 01:54:46 -0600

    Update ping.js v2
    Se agrego el safeExecute

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,11 @@
 const { SlashCommandBuilder } = require('discord.js');
+const safeExecute = require('../utils/safeExecute');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Responde con Pong!'),
-    async execute(interaction) {
+    async execute(interaction , client) {
         await interaction.reply('Pong!');
     },
 };
