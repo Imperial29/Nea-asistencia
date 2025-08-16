a225696cd066d61476f7e6a9e03af15f0953cee1
NoLeoCargado <63430679+Imperial29@users.noreply.github.com>
2025-08-16 11:22:46 -0600

    Update asistencia.js

--- commands/asistencia.js
+++ commands/asistencia.js
@@ -1,5 +1,4 @@
 const { SlashCommandBuilder } = require('discord.js');
-const safeExecute = require('../utils/safeExecute');
 
 // Mapa para guardar los tiempos de inicio, nicks, y el canal por usuario
 const timers = new Map();
@@ -12,25 +11,23 @@ module.exports = {
             option.setName('nick')
                 .setDescription('Proporciona tu Nick')
                 .setRequired(true)),
-    async execute(interaction, client) {
-        await safeExecute(interaction, async (interaction) => {
+    async execute(interaction) {
         const userId = interaction.user.id;
         const nick = interaction.options.getString('nick'); // Obtener el Nick del usuario
         const channelId = interaction.channel.id; // Guardar el canal donde se ejecuta el comando
 
         // Comprobar si ya hay un temporizador corriendo para el usuario
         if (timers.has(userId)) {
-            return interaction.editReply('Ya has iniciado la asistencia.');
+            return interaction.reply('Ya has iniciado la asistencia.');
         }
 
         // Guardar la hora de inicio, el Nick y el Canal
         const startTime = Date.now();
         timers.set(userId, { startTime, nick, channelId });
 
-        await interaction.editReply(`¡Asistencia iniciada! El contador ha comenzado para el Staff: **${nick}**.`);      
-     }); // <-- Cierra safeExecute
-    }, // <-- Cierra la función execute
-}; // <-- Cierra module.exports
+        await interaction.reply(`¡Asistencia iniciada! El contador ha comenzado para el Staff: **${nick}**.`);
+    },
+};
 
 // Exportar el objeto timers para usar en otros comandos
 module.exports.timers = timers;
